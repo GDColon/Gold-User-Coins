d4e24f848eb745511a6fc99b28dd6d8328716920
Colon <56617416+GDColon@users.noreply.github.com>
2024-02-05 18:43:31 -0500

    mm tasty coin

---
+++ README.md
@@ -0,0 +1,3 @@
+# GoldUserCoins
+
+Geode mod that causes all in-game user coins to be rendered as gold secret coins. 100% cosmetic
---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/CCSprite.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MenuLayer) {
+	
+	bool swappedTextures = false;
+
+	bool init() {
+		MenuLayer::init();
+
+		if (m_fields->swappedTextures) return true;
+		else m_fields->swappedTextures = true;
+
+		// swaps all user coin textures with their golden counterparts
+		for (int i = 1; i <= 4; i++) {
+			std::string suffix = ("_00" + std::to_string(i) + ".png");
+			swapTexture("secretCoin_01" + suffix, "secretCoin_2_01" + suffix);
+			swapTexture("secretCoin_b_01" + suffix, "secretCoin_2_b_01" + suffix);
+		}
+
+		// applies a patch that prevents the game from making the particles in coinEffect.plist silver
+		// it's somewhere in EnhancedGameObject::updateUserCoin
+		// first one makes it so (if (isCoin && objectID != 142) { ... }) never runs, 142 is secret coin ID
+		// second and third changes the coin pickup effect to not be silver for user coins 
+		Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x14740a), { 0xEB });
+		Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x14afa8), { 0xEB }); // something 0xc8ffff
+		Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x14b035), { 0xEB }); // something concat21 0xffff
+
+		// todo: android support. but that requires arm knowledge
+
+		return true;
+	} 
+
+	void swapTexture(std::string oldKey, std::string newKey) {
+		auto textureCache = CCSpriteFrameCache::get();
+		auto goldTexture = textureCache->spriteFrameByName(oldKey.c_str());
+		textureCache->removeSpriteFrameByName(newKey.c_str());
+		textureCache->addSpriteFrame(goldTexture, newKey.c_str());
+	}
+};
+
+class $modify(CCSprite) {
+
+	// removes the bronze tint on unverified coins
+	void setColor(cocos2d::ccColor3B const& col) {
+		GameObject* gameObj = typeinfo_cast<GameObject*>(this);
+		if (gameObj && gameObj->m_objectID == 1329) return CCSprite::setColor({255, 255, 255});
+		else return CCSprite::setColor(col);
+	}
+};
